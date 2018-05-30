ecbd59c3ea43d612ba2b94bbca9fa1eff98b6741
Jiecao Yu <jiecaoyu@umich.edu>
2018-05-29 17:35:11 -0700

    fix the bug of computing matrix threshold

--- kernel_example/threshold_gradient_4x4_3x3.py
+++ kernel_example/threshold_gradient_4x4_3x3.py
@@ -1,7 +1,8 @@
-from sympy import * 
-import numpy
+#!/usr/bin/env python2
+from __future__ import absolute_import, division, print_function, unicode_literals
+from sympy import *
 import re
-
+import numpy
 
 for i in range(6):
     for j in range(6):
@@ -15,55 +16,86 @@ W = Matrix([
     [w_4_0, w_4_1, w_4_2, w_4_3, w_4_4, w_4_5],
     [w_5_0, w_5_1, w_5_2, w_5_3, w_5_4, w_5_5],
     ])
+pprint(W)
+
+BT = Matrix([
+    [Rational(4.), Rational( 0.), Rational(-5.), Rational( 0.), Rational(1.), Rational(0.)],
+    [Rational(0.), Rational(-4.), Rational(-4.), Rational( 1.), Rational(1.), Rational(0.)],
+    [Rational(0.), Rational( 4.), Rational(-4.), Rational(-1.), Rational(1.), Rational(0.)],
+    [Rational(0.), Rational(-2.), Rational(-1.), Rational( 2.), Rational(1.), Rational(0.)],
+    [Rational(0.), Rational( 2.), Rational(-1.), Rational(-2.), Rational(1.), Rational(0.)],
+    [Rational(0.), Rational( 4.), Rational( 0.), Rational(-5.), Rational(0.), Rational(1.)]]
+    )
+pprint(BT)
 
 for i in range(6):
     for j in range(6):
-        locals()['x_'+str(i)+'_'+str(j)] = Symbol('x_'+str(i)+'_'+str(j))
+        locals()['i_'+str(i)+'_'+str(j)] = Symbol('i_'+str(i)+'_'+str(j))
 
-X = Matrix([
-    [x_0_0, x_0_1, x_0_2, x_0_3, x_0_4, x_0_5],
-    [x_1_0, x_1_1, x_1_2, x_1_3, x_1_4, x_1_5],
-    [x_2_0, x_2_1, x_2_2, x_2_3, x_2_4, x_2_5],
-    [x_3_0, x_3_1, x_3_2, x_3_3, x_3_4, x_3_5],
-    [x_4_0, x_4_1, x_4_2, x_4_3, x_4_4, x_4_5],
-    [x_5_0, x_5_1, x_5_2, x_5_3, x_5_4, x_5_5],
+I = Matrix([
+    [i_0_0, i_0_1, i_0_2, i_0_3, i_0_4, i_0_5],
+    [i_1_0, i_1_1, i_1_2, i_1_3, i_1_4, i_1_5],
+    [i_2_0, i_2_1, i_2_2, i_2_3, i_2_4, i_2_5],
+    [i_3_0, i_3_1, i_3_2, i_3_3, i_3_4, i_3_5],
+    [i_4_0, i_4_1, i_4_2, i_4_3, i_4_4, i_4_5],
+    [i_5_0, i_5_1, i_5_2, i_5_3, i_5_4, i_5_5],
     ])
+pprint(I)
 
-BT = Matrix([
-    [4.,  0., -5.,  0., 1., 0.],
-    [0., -4., -4.,  1., 1., 0.],
-    [0.,  4., -4., -1., 1., 0.],
-    [0., -2., -1.,  2., 1., 0.],
-    [0.,  2., -1., -2., 1., 0.],
-    [0.,  4.,  0., -5., 0., 1.]]
-    )
-
-
-X_t = BT * X
-X_t = X_t * (BT.transpose())
-
-
-O_t = W.multiply_elementwise(X_t)
+out_tmp = W.multiply_elementwise(BT * I * (BT.transpose()))
 
 AT = Matrix([
-    [1., 1.,  1., 1.,  1., 0.],
-    [0., 1., -1., 2., -2., 0.],
-    [0., 1.,  1., 4.,  4., 0.],
-    [0., 1., -1., 8., -8., 1.]]
+    [Rational(1.), Rational(1.), Rational( 1.), Rational(1.), Rational( 1.), Rational(0.)],
+    [Rational(0.), Rational(1.), Rational(-1.), Rational(2.), Rational(-2.), Rational(0.)],
+    [Rational(0.), Rational(1.), Rational( 1.), Rational(4.), Rational( 4.), Rational(0.)],
+    [Rational(0.), Rational(1.), Rational(-1.), Rational(8.), Rational(-8.), Rational(1.)]]
     )
 
-O = AT * O_t
-O = O * (AT.transpose())
+out = AT * out_tmp * (AT.transpose())
+out = Matrix(out.expand())
 
-O = Matrix(Matrix(O).expand())
+D = numpy.zeros([6,6])
+C = numpy.zeros([6,6])
+for i in range(4 * 4):
+    tmp_out = str(out[i])
+    tmp_out = tmp_out.replace(' ','')
+    weights = {}
+    while len(tmp_out) > 0:
+        item = re.search('[+,-]*[0-9]*\**i_[0-9]*_[0-9]*\*w_[0-9]*_[0-9]*', tmp_out).group(0)
+        numerator = re.search('[+,-]*[0-9]*(?:\*i)', item)
+        if numerator != None:
+            numerator = numerator.group(0)
+            numerator = int(numerator[0:len(numerator)-2])
+        else:
+            if item[0] == '-':
+                numerator = -1
+            else:
+                numerator = 1
+        input_name = re.search('i_[0-9]*_[0-9]*', item).group(0)
+        weight_name = re.search('w_[0-9]*_[0-9]*', item).group(0)
+        if weight_name in weights.keys():
+            if input_name in weights[weight_name].keys():
+                weights[weight_name][input_name] += numerator
+            else:
+                weights[weight_name][input_name] = numerator
+        else:
+            weights[weight_name] = {input_name: numerator}
+        tmp_out = tmp_out[len(item):]
 
-threshold = numpy.zeros([6,6])
-for index in range(16):
-    out = str(O[index])
-    items = re.split(' + | - ', out)
-    for item in items:
-        l = item.split('*')
-        weight = float(l[0]) ** 2.0
-        w_index = l[1].split('_')
-        threshold[int(w_index[1])][int(w_index[2])] += weight
-print threshold**0.5
+    for weight_key in weights.keys():
+        for input_key in weights[weight_key].keys():
+            # D
+            x = int(weight_key.split('_')[1])
+            y = int(weight_key.split('_')[2])
+            D[x][y] += (weights[weight_key][input_key] ** 2.0)
+
+    for weight_key in weights.keys():
+        for input_key1 in weights[weight_key].keys():
+            for input_key2 in weights[weight_key].keys():
+                if input_key1 == input_key2:
+                    continue
+                x = int(weight_key.split('_')[1])
+                y = int(weight_key.split('_')[2])
+                C[x][y] += (weights[weight_key][input_key1] * weights[weight_key][input_key2])
+print(D ** 0.5)
+# print(C)
