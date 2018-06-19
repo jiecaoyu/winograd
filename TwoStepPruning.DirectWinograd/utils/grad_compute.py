ab96208fac2b4fad34780d9c8884f35ea615cb39
Jiecao Yu <jiecaoyu@umich.edu>
2018-06-19 08:57:09 -0700

    directly applied the mask in winograd domain

---
+++ TwoStepPruning.DirectWinograd/utils/grad_compute.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python2
+from __future__ import absolute_import, division, print_function, unicode_literals
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy
+from torch.autograd import Variable
+from . import para
+import os
+import sys
+cwd = os.getcwd()
+sys.path.append(cwd + '/../')
+import newLayers
+from . import para
+
+class GradOptimizer():
+    def __init__(self, model):
+        self.grad_target = []
+        self.opt_operator = []
+        self.kernel_size = []
+        self.input_tile_size = []
+        for m in model.modules():
+            if isinstance(m, newLayers.Winograd2d.Winograd2d):
+                self.grad_target.append(m.weight)
+                self.kernel_size.append(m.kernel_size)
+                self.input_tile_size.append(m.input_tile_size)
+        return
+
+    def step(self):
+        for index in range(len(self.grad_target)):
+            if self.kernel_size[index] == 5:
+                G = torch.from_numpy(para.G_4x4_5x5).float().cuda()
+            elif self.kernel_size[index] == 3:
+                G = torch.from_numpy(para.G_4x4_3x3).float().cuda()
+            input_tile_size = self.input_tile_size[index]
+            grad_target = self.grad_target[index].grad.data
+            GT = G.transpose(0, 1)
+            G = torch.matmul(G, GT)
+            s = grad_target.shape
+            grad_target = grad_target.view(-1, input_tile_size, input_tile_size)
+            grad_target = torch.bmm(G.unsqueeze(0).expand(grad_target.size(0), *G.size()),
+                    grad_target)
+            grad_target = torch.bmm(grad_target,
+                    G.unsqueeze(0).expand(grad_target.size(0), *G.size()))
+            grad_target = grad_target.view(s[0],s[1], input_tile_size, input_tile_size)
+            self.grad_target[index].grad.data = grad_target
+        return
