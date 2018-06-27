6de3063e61ad3877c9b408c2752ae4aff5f2ff49
Jiecao Yu <jiecaoyu@umich.edu>
2018-06-27 11:32:02 -0700

    Winograd Pruning

---
+++ WinogradPruning/utils/grad_compute.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python2
+from __future__ import absolute_import, division, print_function, unicode_literals
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy
+import pickle
+from torch.autograd import Variable
+from . import para
+import os
+import sys
+cwd = os.getcwd()
+sys.path.append(cwd + '/../')
+import newLayers
+from . import para
+
+class GradOptimizer():
+    def __init__(self, model, spatial_mask, prune_list=None):
+        self.grad_target = []
+        self.opt_operator = []
+        self.kernel_size = []
+        self.input_tile_size = []
+        for m in model.modules():
+            if isinstance(m, newLayers.Winograd2d.Winograd2d):
+                self.grad_target.append(m.weight)
+                self.kernel_size.append(m.kernel_size)
+                self.input_tile_size.append(m.input_tile_size)
+        self.spatial_mask = spatial_mask
+        self.prune_list = prune_list
+        if spatial_mask:
+            fp_spatial_mask = open('spatial_mask.pth.tar', 'r')
+            self.spatial_mask = torch.load(fp_spatial_mask)
+            fp_spatial_mask.close()
+        return
+
+    def step(self):
+        count = 0
+        for index in range(len(self.grad_target)):
+            if self.kernel_size[index] == 5:
+                G = torch.from_numpy(para.G_4x4_5x5).float().cuda()
+                mask_multi = torch.from_numpy(para.mask_multi_4x4_5x5).float().cuda()
+            elif self.kernel_size[index] == 3:
+                G = torch.from_numpy(para.G_4x4_3x3).float().cuda()
+                mask_multi = torch.from_numpy(para.mask_multi_4x4_3x3).float().cuda()
+            mask_multi.div_(mask_multi.min())
+            input_tile_size = self.input_tile_size[index]
+            grad_target = self.grad_target[index].grad.data
+            grad_target.div_(mask_multi.pow(2.0).unsqueeze(0).unsqueeze(1))
+            self.grad_target[index].grad.data = grad_target
+            count += 1
+        return
