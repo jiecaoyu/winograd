4f4c47f85989ce37921a6d8ce1acde89afad5b23
Jiecao Yu <jiecaoyu@umich.edu>
2018-08-10 09:23:30 -0700

    scripts for CIFAR10 pruning | need to run more experiments

--- SpatialPruning/CIFAR10/spatial_prune.sh
+++ SpatialPruning/CIFAR10/spatial_prune.sh
@@ -5,4 +5,10 @@ CUDA_VISIBLE_DEVICES=4,5 python main.py --pretrained saved_models/vgg_nagadomi.b
 CUDA_VISIBLE_DEVICES=4,5 python main.py --pretrained saved_models/vgg_nagadomi.prune.0.pth.tar     --prune --winograd-structured --lr 0.01   --epochs 350 --percentage 0.3  --stage 1 &> step1
 CUDA_VISIBLE_DEVICES=4,5 python main.py --pretrained saved_models/vgg_nagadomi.prune.1.pth.tar     --prune --winograd-structured --lr 0.0001 --epochs 150 --percentage 0.4  --stage 2 &> step2
 CUDA_VISIBLE_DEVICES=4,5 python main.py --pretrained saved_models/vgg_nagadomi.prune.2.pth.tar     --prune --winograd-structured --lr 0.01   --epochs 350 --percentage 0.5  --stage 3 &> step3
-CUDA_VISIBLE_DEVICES=4,5 python main.py --pretrained saved_models/vgg_nagadomi.prune.3.pth.tar     --prune --winograd-structured --lr 0.0001 --epochs 150 --percentage 0.55 --stage 4 &> step4
+CUDA_VISIBLE_DEVICES=6,7 python main.py --pretrained saved_models/vgg_nagadomi.prune.3.pth.tar     --prune --winograd-structured --lr 0.001  --epochs 250 --percentage 0.55 --stage 4 &> step4
+
+CUDA_VISIBLE_DEVICES=6,7 python main.py --pretrained saved_models/vgg_nagadomi.prune.4.pth.tar     --prune --winograd-structured --lr 0.001  --epochs 250 --percentage 0.60 --stage 5 &> step5
+CUDA_VISIBLE_DEVICES=6,7 python main.py --pretrained saved_models/vgg_nagadomi.prune.5.pth.tar     --prune --winograd-structured --lr 0.001  --epochs 250 --percentage 0.65 --stage 6 &> step6
+CUDA_VISIBLE_DEVICES=6,7 python main.py --pretrained saved_models/vgg_nagadomi.prune.6.pth.tar     --prune --winograd-structured --lr 0.001  --epochs 250 --percentage 0.70 --stage 7 &> step7
+CUDA_VISIBLE_DEVICES=6,7 python main.py --pretrained saved_models/vgg_nagadomi.prune.7.pth.tar     --prune --winograd-structured --lr 0.001  --epochs 250 --percentage 0.75 --stage 8 &> step8
+CUDA_VISIBLE_DEVICES=6,7 python main.py --pretrained saved_models/vgg_nagadomi.prune.8.pth.tar     --prune --winograd-structured --lr 0.001  --epochs 250 --percentage 0.80 --stage 9 &> step9
