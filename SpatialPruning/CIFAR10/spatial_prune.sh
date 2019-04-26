4bdc9f4439ba14ae61d728ce12d65f6037af7f75
jiecaoyu <jiecaoyu@umich.edu>
2019-04-26 15:09:03 -0400

    update CIFAR-10 spatial pruning

--- SpatialPruning/CIFAR10/spatial_prune.sh
+++ SpatialPruning/CIFAR10/spatial_prune.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
-CUDA_VISIBLE_DEVICES=4,5 python main.py &> baseline.log
+python main.py &> baseline.log
 
-CUDA_VISIBLE_DEVICES=4,5 python main.py --pretrained saved_models/vgg_nagadomi.best_origin.pth.tar --prune --winograd-structured --lr 0.0001 --epochs 150 --percentage 0.2  --stage 0 &> step0
-CUDA_VISIBLE_DEVICES=4,5 python main.py --pretrained saved_models/vgg_nagadomi.prune.0.pth.tar     --prune --winograd-structured --lr 0.01   --epochs 350 --percentage 0.3  --stage 1 &> step1
-CUDA_VISIBLE_DEVICES=4,5 python main.py --pretrained saved_models/vgg_nagadomi.prune.1.pth.tar     --prune --winograd-structured --lr 0.0001 --epochs 150 --percentage 0.4  --stage 2 &> step2
-CUDA_VISIBLE_DEVICES=4,5 python main.py --pretrained saved_models/vgg_nagadomi.prune.2.pth.tar     --prune --winograd-structured --lr 0.01   --epochs 350 --percentage 0.5  --stage 3 &> step3
-CUDA_VISIBLE_DEVICES=6,7 python main.py --pretrained saved_models/vgg_nagadomi.prune.3.pth.tar     --prune --winograd-structured --lr 0.001  --epochs 250 --percentage 0.55 --stage 4 &> step4
+python main.py --pretrained saved_models/vgg_nagadomi.best_origin.pth.tar --prune --winograd-structured --lr 0.0001 --epochs 150 --percentage 0.2  --stage 0 &> step0
+python main.py --pretrained saved_models/vgg_nagadomi.prune.0.pth.tar     --prune --winograd-structured --lr 0.01   --epochs 350 --percentage 0.3  --stage 1 &> step1
+python main.py --pretrained saved_models/vgg_nagadomi.prune.1.pth.tar     --prune --winograd-structured --lr 0.01   --epochs 350 --percentage 0.4  --stage 2 &> step2
+python main.py --pretrained saved_models/vgg_nagadomi.prune.2.pth.tar     --prune --winograd-structured --lr 0.01   --epochs 350 --percentage 0.5  --stage 3 &> step3
+python main.py --pretrained saved_models/vgg_nagadomi.prune.3.pth.tar     --prune --winograd-structured --lr 0.01   --epochs 350 --percentage 0.55 --stage 4 &> step4
 
-CUDA_VISIBLE_DEVICES=6,7 python main.py --pretrained saved_models/vgg_nagadomi.prune.4.pth.tar     --prune --winograd-structured --lr 0.001  --epochs 250 --percentage 0.60 --stage 5 &> step5
-CUDA_VISIBLE_DEVICES=6,7 python main.py --pretrained saved_models/vgg_nagadomi.prune.5.pth.tar     --prune --winograd-structured --lr 0.001  --epochs 250 --percentage 0.65 --stage 6 &> step6
-CUDA_VISIBLE_DEVICES=6,7 python main.py --pretrained saved_models/vgg_nagadomi.prune.6.pth.tar     --prune --winograd-structured --lr 0.001  --epochs 250 --percentage 0.70 --stage 7 &> step7
-CUDA_VISIBLE_DEVICES=6,7 python main.py --pretrained saved_models/vgg_nagadomi.prune.7.pth.tar     --prune --winograd-structured --lr 0.001  --epochs 250 --percentage 0.75 --stage 8 &> step8
-CUDA_VISIBLE_DEVICES=6,7 python main.py --pretrained saved_models/vgg_nagadomi.prune.8.pth.tar     --prune --winograd-structured --lr 0.001  --epochs 250 --percentage 0.80 --stage 9 &> step9
+python main.py --pretrained saved_models/vgg_nagadomi.prune.4.pth.tar     --prune --winograd-structured --lr 0.01   --epochs 350 --percentage 0.60 --stage 5 &> step5
+python main.py --pretrained saved_models/vgg_nagadomi.prune.5.pth.tar     --prune --winograd-structured --lr 0.01   --epochs 350 --percentage 0.65 --stage 6 &> step6
+python main.py --pretrained saved_models/vgg_nagadomi.prune.6.pth.tar     --prune --winograd-structured --lr 0.01   --epochs 350 --percentage 0.70 --stage 7 &> step7
+python main.py --pretrained saved_models/vgg_nagadomi.prune.7.pth.tar     --prune --winograd-structured --lr 0.01   --epochs 350 --percentage 0.75 --stage 8 &> step8
+python main.py --pretrained saved_models/vgg_nagadomi.prune.8.pth.tar     --prune --winograd-structured --lr 0.01   --epochs 350 --percentage 0.80 --stage 9 &> step9
+python main.py --pretrained saved_models/vgg_nagadomi.prune.9.pth.tar     --prune --winograd-structured --lr 0.01   --epochs 350 --percentage 0.85 --stage 10 &> step10
+python main.py --pretrained saved_models/vgg_nagadomi.prune.10.pth.tar    --prune --winograd-structured --lr 0.01   --epochs 350 --percentage 0.90 --stage 11 &> step11
