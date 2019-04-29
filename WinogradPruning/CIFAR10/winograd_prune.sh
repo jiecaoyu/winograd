6f1a569949281f47466294693b49e2ea92359103
Jiecao Yu <jiecaoyu@umich.edu>
2019-04-29 06:09:29 +0000

    CIFAR-10 pruning

--- WinogradPruning/CIFAR10/winograd_prune.sh
+++ WinogradPruning/CIFAR10/winograd_prune.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-python winograd.py --pretrained-normal vgg_nagadomi.prune.5.pth.tar                       --prune --percentage 0.63 --lr 0.000001 --stage 0 --weight-decay 0.0003 &> step0
-python winograd.py --pretrained-normal saved_models/vgg_nagadomi_winograd.prune.0.pth.tar --prune --percentage 0.66 --lr 0.000001 --stage 1 --weight-decay 0.0003 &> step1
-python winograd.py --pretrained-normal saved_models/vgg_nagadomi_winograd.prune.1.pth.tar --prune --percentage 0.69 --lr 0.000001 --stage 2 --weight-decay 0.0003 &> step2
-python winograd.py --pretrained-normal saved_models/vgg_nagadomi_winograd.prune.2.pth.tar --prune --percentage 0.72 --lr 0.000001 --stage 3 --weight-decay 0.0003 &> step3
-python winograd.py --pretrained-normal saved_models/vgg_nagadomi_winograd.prune.3.pth.tar --prune --percentage 0.75 --lr 0.000001 --stage 4 --weight-decay 0.0003 &> step4
+python winograd.py --pretrained-normal ../../SpatialPruning/CIFAR10/saved_models/vgg_nagadomi.prune.7.pth.tar --prune --percentage  0.73 --lr 0.000001 --stage 0 --weight-decay 0.0003 &> step0
+python winograd.py --pretrained-normal saved_models/vgg_nagadomi_winograd.prune.0.pth.tar --prune --percentage 0.76 --lr 0.000001 --stage 1 --weight-decay 0.0003 &> step1
+python winograd.py --pretrained-normal saved_models/vgg_nagadomi_winograd.prune.1.pth.tar --prune --percentage 0.79 --lr 0.000001 --stage 2 --weight-decay 0.0003 &> step2
+python winograd.py --pretrained-normal saved_models/vgg_nagadomi_winograd.prune.2.pth.tar --prune --percentage 0.82 --lr 0.000001 --stage 3 --weight-decay 0.0003 &> step3
